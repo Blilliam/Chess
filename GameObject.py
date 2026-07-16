a4ae3534ce251b2924f65be50db162cda743b539
William Scudder <wtscudder@Williams-MacBook-Air.local>
2026-07-16 10:17:04 -0400

    7/16/26 - 1

--- GameObject.py
+++ GameObject.py
@@ -4,7 +4,6 @@ from Board import Board
 import Constants
 from Constants import *
 import Piece
-import copy
 
 
 class GameObject:
@@ -40,14 +39,83 @@ class GameObject:
                 if tile.isOccupied():
                     tile.piece.isSelected = False
 
-        
+    def getKingOnBoard(self, board, team) -> Piece:
+        for row in board.board:
+            for tile in row:
+                if tile.isOccupied() and isinstance(tile.piece, Piece.King) and tile.piece.team == team:
+                    return tile.piece
+        return None
+
+    def createBoardSnapshot(self, sourceBoard=None):
+        boardToCopy = self.mainBoard if sourceBoard is None else sourceBoard
+        snapshotBoard = Board()
+        for row in range(8):
+            for col in range(8):
+                sourceTile = boardToCopy.board[row][col]
+                targetTile = snapshotBoard.board[row][col]
+                if sourceTile.isOccupied():
+                    piece = sourceTile.piece
+                    targetTile.putPiece(piece)
+                    piece.mainBoard = snapshotBoard
+                    piece.x = col
+                    piece.y = row
+        return snapshotBoard
+
+    def isBoardInCheck(self, board, king) -> bool:
+        if king is None:
+            return False
+
+        allOppMoves = []
+        for row in board.board:
+            for tile in row:
+                if tile.isOccupied() and king.team == Constants.getOppColor(tile.piece.team):
+                    for move in tile.piece.getMoves():
+                        allOppMoves.append(tuple(move))
+
+        return (king.y, king.x) in allOppMoves
+
+    def getLegalMoves(self, piece):
+        self.pastBoard = self.createBoardSnapshot(self.mainBoard)
+        legalMoves = []
+
+        for move in piece.getMoves():
+            row, col = move
+            if not (0 <= row < 8 and 0 <= col < 8):
+                continue
+
+            simulatedBoard = self.createBoardSnapshot(self.pastBoard)
+            originTile = simulatedBoard.board[piece.y][piece.x]
+            targetTile = simulatedBoard.board[row][col]
+            movingPiece = originTile.piece
+
+            if targetTile.isOccupied() and targetTile.piece.team == movingPiece.team:
+                continue
+
+            originalX = movingPiece.x
+            originalY = movingPiece.y
+            originalBoard = movingPiece.mainBoard
+            targetTile.putPiece(None)
+            targetTile.putPiece(movingPiece)
+            originTile.putPiece(None)
+            movingPiece.x = targetTile.x
+            movingPiece.y = targetTile.y
+
+            king = self.getKingOnBoard(simulatedBoard, piece.team)
+            if not self.isBoardInCheck(simulatedBoard, king):
+                legalMoves.append(move)
+
+            movingPiece.mainBoard = originalBoard
+            movingPiece.x = originalX
+            movingPiece.y = originalY
+
+        return legalMoves
 
     def selectPiece(self, piece):
         self.clearSelectionAndMoves()
         self.cPiece = piece
         self.cPiece.isSelected = True
 
-        for move in self.cPiece.getMoves():
+        for move in self.getLegalMoves(piece):
             row, col = move
             if 0 <= row < 8 and 0 <= col < 8:
                 self.mainBoard.board[row][col].isMoveable = True
@@ -62,8 +130,8 @@ class GameObject:
         clickedTile = self.mainBoard.board[cY][cX]
         tempPiece = self.mainBoard.getPeice(cX, cY)
 
-        if (self.isInCheck()):
-            self.pastBoard = copy.deepcopy(self.mainBoard)
+        if self.isInCheck(self.getKing(self.turn)):
+            self.pastBoard = self.createBoardSnapshot(self.mainBoard)
 
         if self.cPiece != None and (clickedTile.isMoveable) and self.cPiece.team == self.turn:
             originTile = self.mainBoard.board[self.cPiece.y][self.cPiece.x]
@@ -98,16 +166,21 @@ class GameObject:
                     return tile.piece
 
 
-    def isInCheck(self, king) -> bool:
+    def isInCheck(self, king=None) -> bool:
+        if king is None:
+            king = self.getKing(self.turn)
+
+        if king is None:
+            return False
+
         allOppMoves = []
         for row in self.mainBoard.board:
             for tile in row:
-                if (tile.isOccupied() and king.team == Constants.getOppColor(tile.piece.team)):
+                if tile.isOccupied() and king.team == Constants.getOppColor(tile.piece.team):
                     for move in tile.piece.getMoves():
-                        allOppMoves.append(move)
+                        allOppMoves.append(tuple(move))
         for y, x in allOppMoves:
-            if (king.x == x and king.y == y):
-                print("in check")
+            if king.x == x and king.y == y:
                 return True
         return False
 
